641ca684dbf8376b45e7fb94c1c13f9027ffdf47
Jason Snider <root@jasonsnider.com>
2018-12-12 11:52:40 -0600

    Added the ability to reject unwanted service commands

--- re.sh
+++ re.sh
@@ -9,13 +9,19 @@ then
     exit 1
 fi
 
-# Move the current execution state to the proper directory
-cd /etc/apache2/sites-available
+if [ "$COMMAND" == "reload" ] || [ "$COMMAND" == "restart" ]
+then
+    # Move the current execution state to the proper directory
+    cd /etc/apache2/sites-available
 
-# Disable a vhost configuration
-sudo a2dissite "$CONFIG"
-sudo service apache2 "$COMMAND"
+    # Disable a vhost configuration
+    sudo a2dissite "$CONFIG"
+    sudo service apache2 "$COMMAND"
 
-# Enable a vhost configuration
-sudo a2ensite "$CONFIG"
-sudo service apache2 "$COMMAND"
+    # Enable a vhost configuration
+    sudo a2ensite "$CONFIG"
+    sudo service apache2 "$COMMAND"
+else
+    echo "ERROR: $COMMAND is not a valid service directive {reload|restart}"
+    exit 1
+fi
